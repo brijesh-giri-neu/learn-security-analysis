1dc3c4b7082689c461eb0f9ef3dade6d646b3209
brijesh-giri-neu <giri.b@northeastern.edu>
2025-04-04 20:09:19 -0400

    Fix: STRIDE Threats

--- pages/book_details.ts
+++ pages/book_details.ts
@@ -2,6 +2,8 @@ import Book  from '../models/book';
 import BookInstance, { IBookInstance }  from '../models/bookinstance';
 import express from 'express';
 
+import { validateIdMiddleware, RequestWithSanitizedId } from '../sanitizers/idSanitizer'
+
 const router = express.Router();
 
 
@@ -13,14 +15,13 @@ const router = express.Router();
  * @returns 404 - if the book is not found
  * @returns 500 - if there is an error in the database
  */
-router.get('/', async (req, res) => {
-  const id = req.query.id as string;
+router.get('/', validateIdMiddleware, async (req: RequestWithSanitizedId, res) => {
+  const id = req.sanitizedId as string;
   try {
     const [book, copies] = await Promise.all([
       Book.getBook(id),
       BookInstance.getBookDetails(id)
     ]);
-
     if (!book) {
       res.status(404).send(`Book ${id} not found`);
       return;
